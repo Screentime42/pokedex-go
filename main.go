965f06a033d76d7cfc6fe410271ffbe4999be55c
Screentime42 <danohare99@googlemail.com>
2025-11-06 12:20:55 +0000

    Added cliCommand registry and exit command. Additionally, removed some testing logic

--- main.go
+++ main.go
@@ -3,10 +3,36 @@ package main
 import (
 	"fmt"
 	"strings"
+	"bufio"
+	"os"
 )
 
 func main() {
-    fmt.Println("Hello, World!")
+   scanner := bufio.NewScanner(os.Stdin)
+	fmt.Println("Welcome to your Pokedex. Type exit to quit.")
+
+	for {
+		fmt.Print("Pokedex > ")
+		if !scanner.Scan() {
+			fmt.Fprintln(os.Stderr, "Incorrect input.")
+			break
+		}
+
+		input := strings.TrimSpace(scanner.Text())
+		words := cleanInput(input)
+		cmdName := words[0]
+		if cmd, ok := commands[cmdName]; ok {
+			cmd.callback()
+		} else {
+			fmt.Println("Unknown command")
+		}
+
+		
+
+		if err := scanner.Err(); err != nil {
+        fmt.Fprintln(os.Stderr, "Error reading input:", err)
+    }
+	}
 }
 
 
@@ -16,4 +42,25 @@ func cleanInput(text string) []string {
 		words[i] = strings.ToLower(words[i])
 	}
 	return words
+}
+
+
+type cliCommand struct {
+		name			string
+		description	string
+		callback		func()
+}
+
+
+var commands = map[string]cliCommand {
+	"exit": {
+		name:				"exit",
+		description:	"Exit the Pokedex",
+		callback:		commandExit,
+	},
+}
+
+func commandExit() {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
 }
